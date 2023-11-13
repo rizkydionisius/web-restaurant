e63ebd22b85d7f138e6e64cf492efcee454e1a37
Dion4286 <dionpramu@students.amikom.ac.id>
2023-11-13 07:14:57 +0700

    menambahkan card, footer

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -38,3 +38,40 @@ function adjustHeroImageSize() {
         heroImage.style.minWidth = "0";
     }
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Ambil data restoran dari file DATA.json
+    fetch("data/DATA.json")
+        .then(response => response.json())
+        .then(data => {
+            // Panggil fungsi untuk menampilkan daftar restoran
+            renderRestaurantList(data.restaurants);
+        })
+        .catch(error => console.error("Error fetching data:", error));
+});
+
+function renderRestaurantList(restaurants) {
+    const restaurantListContainer = document.getElementById("restaurantList");
+
+    // Loop melalui setiap restoran dan buat elemen card
+    restaurants.forEach(restaurant => {
+        const card = document.createElement("div");
+        card.classList.add("restaurant-card");
+
+        // Menambahkan elemen-elemen ke dalam card 
+        card.innerHTML = `
+            <img src="${restaurant.pictureId}" alt="${restaurant.name}" class="restaurant-image">
+            <div class="restaurant-info">
+                <h2>${restaurant.name}</h2>
+                <p>${restaurant.city}</p>
+                <p>Rating: ${restaurant.rating}</p>
+                <p>${restaurant.description}</p>
+            </div>
+        `;
+
+        // Tambahkan card ke dalam container
+        restaurantListContainer.appendChild(card);
+    });
+}
+
+
